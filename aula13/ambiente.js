2e05efd4fbc54eebeb66980df3077a38d34ec8c1
André Inoue <118934334+andreyyinoue@users.noreply.github.com>
2023-01-31 11:12:45 -0300

    Aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+let c = 0
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+// let c = 0
+// while (c < 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
+
+
+
+// console.log("Tudo bem?")
+
