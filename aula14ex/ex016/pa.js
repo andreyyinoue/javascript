15c23614c31f4673c932c33f2d52beba6629f09e
André Inoue <118934334+andreyyinoue@users.noreply.github.com>
2023-01-31 22:03:56 -0300

    ex016

--- aula14ex/ex016/pa.js
+++ aula14ex/ex016/pa.js
@@ -1,29 +1,7 @@
-function load(){
-    let msg = document.getElementById('msg')
-    let img = document.getElementById('foto')
-    let mensagem = document.getElementById('aisatsu')
-    let data = new Date()
-    let hora = data.getHours()
-    //let hora = 
-    let min = data.getMinutes()
-    if (min <10) {
-        min = "0" + min
-    } 
-    
+function calc() {
+    let a1 = document.getElementById('ia1')
+    let an = document.getElementById('ian')
+    let r = document.getElementById('ir')
 
-    if (hora >= 0 && hora < 12) {
-        img.src = 'morning.jpg'
-        document.body.style.background = '#e2cd9f'
-        mensagem.innerHTML = "Bom dia"
-    } else if(hora >= 12 && hora < 18) {
-        img.src = 'afternoon.jpg'
-        document.body.style.background = '#b9846f'
-        mensagem.innerHTML = "Boa Tarde"
-    } else {
-        img.src = 'night.jpg'
-        document.body.style.background = '#515154'
-        mensagem.innerHTML = "Boa Noite"
-    }
-    msg.innerHTML = `Agora são ${hora}:${min} horas`
+    if (Int16Array.value.length == 0 || )
 }
- 
