e8749f9f15077c35d4e2f8636dd1ccbde975be38
André Inoue <118934334+andreyyinoue@users.noreply.github.com>
2023-02-01 15:43:46 -0300

    Ex017

--- aula14ex/ex017/tabuada.js
+++ aula14ex/ex017/tabuada.js
@@ -1,28 +1,10 @@
-function load(){
-    let msg = document.getElementById('msg')
-    let img = document.getElementById('foto')
-    let mensagem = document.getElementById('aisatsu')
-    let data = new Date()
-    let hora = data.getHours()
-    //let hora = 
-    let min = data.getMinutes()
-    if (min <10) {
-        min = "0" + min
-    } 
-    
-
-    if (hora >= 0 && hora < 12) {
-        img.src = 'morning.jpg'
-        document.body.style.background = '#e2cd9f'
-        mensagem.innerHTML = "Bom dia"
-    } else if(hora >= 12 && hora < 18) {
-        img.src = 'afternoon.jpg'
-        document.body.style.background = '#b9846f'
-        mensagem.innerHTML = "Boa Tarde"
+function showtab() {
+    let num = document.getElementById('inum')
+    let tab = document.getElementsById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Digite um número')
     } else {
-        img.src = 'night.jpg'
-        document.body.style.background = '#515154'
-        mensagem.innerHTML = "Boa Noite"
+        let n = Number(num.value)
     }
-    msg.innerHTML = `Agora são ${hora}:${min} horas`
+     
 }
